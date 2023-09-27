6c4acf8cb219ba876e40611a6a76998a23147594
adaboubvincent <adaboubvincent@gmail.com>
2023-09-27 08:05:49 +0000

    Created utils functions

---
+++ cosomis/kobotoolbox/utils.py
@@ -0,0 +1,87 @@
+from datetime import datetime, date
+
+from kobotoolbox.api_call import get_all
+from administrativelevels.models import AdministrativeLevel
+from subprojects.models import Subproject, SubprojectFile
+from kobotoolbox.form_id_kobo import FORM_ID_KOBO_GMS
+
+
+def kobo_util():
+    all_forms = get_all()
+    print(all_forms)
+    counter = 1
+    for a in all_forms['results']:
+        village = AdministrativeLevel.objects.filter(name=a['Village'])
+        if village:
+            village_object = village.first()
+            coordinates = a['gps'].split(' ')
+            print(counter, village_object.name, a['Village'], coordinates)
+            village_object.latitude = coordinates[0]
+            village_object.longitude = coordinates[1]
+            village_object.save()
+        counter = counter + 1
+    
+
+
+def get_gms_form_reponse_util():
+    all_forms = get_all(FORM_ID_KOBO_GMS)
+    
+    counter = 1
+    for a in all_forms['results']:
+        if a['type_georeferencement'] == "ouvrages_coso":
+            subprojects =  Subproject.objects.filter(number=int(float(a['serial'])))
+            if subprojects:
+                subproject = subprojects.first()
+                coordinates = a['gps'].split(' ')
+                subproject.latitude = coordinates[0]
+                subproject.longitude = coordinates[1]
+                subproject.save()
+            counter += 1
+        
+    print(counter-1)
+    print(Subproject.objects.filter(latitude__isnull=True).count())
+        
+    
+
+
+def get_gms_form_reponse_save_images_util():
+    all_forms = get_all(FORM_ID_KOBO_GMS)
+    
+    counter = 1
+    for a in all_forms['results']:
+        if a['type_georeferencement'] == "ouvrages_coso":
+            subprojects =  Subproject.objects.filter(number=int(float(a['serial'])))
+            if subprojects:
+                subproject = subprojects.first()
+                
+                c = 0
+                for attachment in sorted(a['_attachments'], key=lambda obj: obj.get('id')):
+                    imgs = SubprojectFile.objects.filter(url=attachment['download_url'])
+                    if imgs.exists():
+                        img = imgs.first()
+                    else:
+                        img = SubprojectFile()
+                        img.subproject = subproject
+                        img.principal = True if c == 0 else False
+                        img.order = subproject.get_all_images().count() + 1
+
+                    img.name = a['description_niveau_travaux']
+                    img.url = attachment['download_url']
+                    try:
+                        if a.get('_submission_time'):
+                            _date = a['_submission_time'].split('T')[0]
+                            date_split = _date.split('-')
+                            img.date_taken =  date(int(date_split[0]), int(date_split[1]), int(date_split[2])) #datetime.strptime(a['_submission_time'], '%Y-%m-%d')
+                    except:
+                        pass
+                    if not img.date_taken:
+                        img.date_taken = datetime.now()
+                    img.save()
+                    c += 1
+            counter += 1
+            
+        
+    print(counter-1)
+    print(Subproject.objects.filter(latitude__isnull=True).count())
+        
+    
