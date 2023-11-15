26d94eb3bbe89bd0bf7dbdf687d0bfe84bd77514
adaboubvincent <adaboubvincent@gmail.com>
2023-11-15 09:24:17 +0000

    Updated django-admin adl search

--- cosomis/administrativelevels/admin.py
+++ cosomis/administrativelevels/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import AdministrativeLevel, GeographicalUnit, CVD
 
+
+class AdministrativeLevelAdmin(admin.ModelAdmin):
+    search_fields = [
+        'name',
+        'parent__name',
+        'no_sql_db_id'
+    ]
+
+
 # Register your models here.
-admin.site.register(AdministrativeLevel)
+admin.site.register(AdministrativeLevel, AdministrativeLevelAdmin)
 admin.site.register(GeographicalUnit)
 admin.site.register(CVD)
